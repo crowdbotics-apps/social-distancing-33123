8a8bfce80e6d6c4e7acb49e6c556f0236c09ecf6
Crowdbotics <team@crowdbotics.com>
2022-01-24 07:39:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "social_distancing_33123",
+  url: "https://social_distancing_33123.botics.co",
+  api: "https://social_distancing_33123.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
